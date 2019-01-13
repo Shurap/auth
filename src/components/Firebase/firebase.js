d0df69bfed26de7c16c36244e9dd53d32e3f17a0
Shur_ap <bap.git@mail.ru>
2019-01-13 19:07:25 +0300

    before env

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,6 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+
 const config = {
   apiKey: "AIzaSyAfX2DUD-F-aduyKLoWg6qPA8Tv2qCaF-4",
   authDomain: "auth-b3296.firebaseapp.com",
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import Firebase, {FirebaseContext} from './components/Firebase'
+require('dotenv').config();
+
+console.log('---------', process.env.DB_USER);
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
