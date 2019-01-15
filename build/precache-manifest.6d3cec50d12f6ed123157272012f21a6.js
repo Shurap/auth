b8247edd93746b3bc70d0a4456f04d6e56f2066b
Shur_ap <bap.git@mail.ru>
2019-01-15 22:15:44 +0300

    initial commit

---
+++ build/precache-manifest.6d3cec50d12f6ed123157272012f21a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07e1b1a0868c2066b3bb",
+    "url": "/static/js/main.07e1b1a0.chunk.js"
+  },
+  {
+    "revision": "b9b39b9b07265e501130",
+    "url": "/static/js/1.b9b39b9b.chunk.js"
+  },
+  {
+    "revision": "07e1b1a0868c2066b3bb",
+    "url": "/static/css/main.72795c5b.chunk.css"
+  },
+  {
+    "revision": "6eba615b3f07edfb477293a6350c5190",
+    "url": "/index.html"
+  }
+];
